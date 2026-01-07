7be090a165700b0867d056417f77432b395c162a
Matt Russo <matthew.russo91@gmail.com>
2026-01-06 19:10:31 -0500

    complete exercise 1.11: unicafe step 6

--- part1/notes.md
+++ part1/notes.md
@@ -211,4 +211,5 @@ Total time spent on part 1d lecture material: 1 hour
 * 1.8: unicafe step 3 => 5 minutes
 * 1.9: unicafe step 4 => 5 minutes
 * 1.10: unicafe step 5 => 15 minutes
+* 1.11: unicafe step 6 => 20 minutes
 
--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,7 +2,14 @@ import { useState } from 'react';
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
-const StatisticLine = ({ text, value }) => <p>{text}: {value}</p>
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
 
 const Statistics = ({ good, neutral, bad }) => {
   const all = good + neutral + bad;
@@ -18,14 +25,16 @@ const Statistics = ({ good, neutral, bad }) => {
   }
 
   return (
-    <div>
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="all" value={all} />
-      <StatisticLine text="average" value={avg} />
-      <StatisticLine text="positive" value={percentPositive} />
-    </div>
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={all} />
+        <StatisticLine text="average" value={avg} />
+        <StatisticLine text="positive" value={`${percentPositive} %`} />
+      </tbody>
+    </table>
   )
 }
 
@@ -42,7 +51,10 @@ const App = () => {
         <Button onClick={() => setNeutral(neutral + 1)} text='neutral' />
         <Button onClick={() => setBad(bad + 1)} text='bad' />
       </div>
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <div>
+        <h2>Statistics</h2>
+        <Statistics good={good} neutral={neutral} bad={bad} />
+      </div>
     </>
   )
 }
