052c572336aa302ce2ed00bf87e32e0d4fa8dc00
Matt Russo <matthew.russo91@gmail.com>
2026-01-26 11:29:12 -0500

    exercise 3.8: Phonebook backend step 8

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -5,7 +5,12 @@ const app = express()
 
 // Middleware
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('req-body', (req, res) => {
+  return JSON.stringify(req.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'))
 
 // const requestLogger = (request, response, next) => {
 //   console.log('Method:', request.method)
