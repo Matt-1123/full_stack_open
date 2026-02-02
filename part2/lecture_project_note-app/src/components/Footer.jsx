8e8430271053d8323d6ace20cf1ee68d7c494d47
Matt Russo <matthew.russo91@gmail.com>
2026-02-01 20:32:48 -0500

    add proxy for api

--- part2/lecture_project_note-app/src/components/Footer.jsx
+++ part2/lecture_project_note-app/src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <div style={footerStyle}>
       <br />
       <p>
-        Note app, Department of Computer Science, University of Helsinki 2025
+        Notes app, Department of Computer Science, University of Helsinki 2025
       </p>
       <p>The script npm run build:ui builds the frontend and copies the production version under the backend repository. The script npm run deploy releases the current backend to Render.</p>
     </div>
