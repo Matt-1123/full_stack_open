3b4defe27fe122aa39d274b1e698155971874ded
Matt Russo <matthew.russo91@gmail.com>
2026-02-20 13:36:31 -0500

    remove db config from index.js (it is already set in module note.js)

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -7,48 +7,12 @@ const generateId = require('./utils/generateId')
 
 const app = express()
 
-const password = process.argv[2]
-const url = process.env.MONGODB_URI
-
 mongoose.set('strictQuery',false)
 mongoose.connect(url, { family: 4 })
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-})
-
-noteSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-const Note = mongoose.model('Note', noteSchema)
 
 app.use(express.json())
 app.use(express.static('dist')) //show static content - whenever Express gets an HTTP GET request it will first check if the dist directory contains a file corresponding to the request's address. If a correct file is found, Express will return it.
 app.use(cors())
-
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
