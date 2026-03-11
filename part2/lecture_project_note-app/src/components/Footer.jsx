7f200b0f12c5af47d5825b8a9ebe8aa0e52334a8
Matt Russo <matthew.russo91@gmail.com>
2026-03-10 21:35:40 -0400

    update year

--- part2/lecture_project_note-app/src/components/Footer.jsx
+++ part2/lecture_project_note-app/src/components/Footer.jsx
@@ -8,8 +8,7 @@ const Footer = () => {
     <div style={footerStyle}>
       <br />
       <p>
-        Notes app, Department of Computer Science, University of Helsinki 2025
-      </p>
+        Notes app, Department of Computer Science, University of Helsinki 2026      </p>
       <p>The script npm run build:ui builds the frontend and copies the production version under the backend repository. The script npm run deploy releases the current backend to Render.</p>
     </div>
   )
